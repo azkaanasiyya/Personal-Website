58890f776aba18a3164f30cf35033db82618badc
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-04-14 13:13:58 +0700

    fix: update bug this app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/templates/Navbar.jsx'
 import Hero from './components/sections/Hero.jsx'
-import Project from './components/sections/Project.jsx'
+// import Project from './components/sections/Project.jsx'
 
 function App() {
 
