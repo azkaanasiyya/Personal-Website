3e19455e9de98a3287bfaa9a3cd97962b0e43732
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-04-22 14:02:29 +0700

    fix(navbar): enable smooth scrolling to section when menu link is clicked

--- src/components/templates/Navbar.jsx
+++ src/components/templates/Navbar.jsx
@@ -1,31 +1,38 @@
-// import logo from '../../assets/logo.png';
 import logoWhite from '../../assets/logo-white.png';
 
 function Navbar() {
+  const handleSmoothScroll = (e, targetID) => {
+    e.preventDefault();
+    const target = document.querySelector(targetID);
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
   return (
-    <div className="navbar w-full bg-neutral-500">
-      <div className="container mx-auto max-w-[1200px] px-5 py-6 lg:px-8 lg:py-8">
+    <div className="navbar sticky top-0 z-[9999] w-full bg-neutral-500">
+      <div className="container mx-auto max-w-[1200px] px-5 py-6">
         <div className="navbar-wrapper flex items-center justify-between">
           <div className="logo flex max-w-[138px] items-center space-x-4 font-semibold text-primary-500 lg:max-w-full">
             <a href="#hero">
-              <img src={logoWhite} alt="logo" className='h-10' />
+              <img src={logoWhite} alt="logo" className="h-10" />
             </a>
           </div>
 
           <div className="navbar-block hidden lg:flex">
             <ul className="flex gap-[44px] text-base font-medium text-white">
               <li>
-                <a href="#plans" className="hover:text-primary-500">
+                <a href="#about" onClick={(e) => handleSmoothScroll(e, '#about')} className="hover:text-primary-500">
                   About
                 </a>
               </li>
               <li>
-                <a href="#plans" className="hover:text-primary-500">
+                <a href="#project" onClick={(e) => handleSmoothScroll(e, '#project')} className="hover:text-primary-500">
                   Projects
                 </a>
               </li>
               <li>
-                <a href="#testimony" className="hover:text-primary-500">
+                <a href="#skill" onClick={(e) => handleSmoothScroll(e, '#skill')} className="hover:text-primary-500">
                   Skills
                 </a>
               </li>
